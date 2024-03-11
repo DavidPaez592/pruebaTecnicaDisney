eb4b727be20b65e02052250b16337637c792dc3f
DavidPaez592 <Davidpaez502@gmail.com>
2024-03-11 09:46:03 -0500

    "Prueba"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router basename="/disney-app">
-    <React.StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
       <App />
-    </React.StrictMode>
-  </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
